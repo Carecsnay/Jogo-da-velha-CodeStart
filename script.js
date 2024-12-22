83c984794e1a7c803f94279242f1d52f1ffb5c2a
Bruno Desktop <carecsnay1@gmail.com>
2024-12-22 18:22:58 -0300

    Feat: swap between players.

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 const dqs = (element) => document.querySelector(element);
+const dqsa = (element) => document.querySelectorAll(element);
+
+let isCircleTurn = false;
+
+const cellElements = dqsa("[data-cell]");
+const boardElement = dqs("[data-board]");
+
+// Marca a celular com x ou circulo (adionando a class)
+const placeMark = (cell, classToAdd) => {
+    cell.classList.add(classToAdd);
+
+    boardElement.classList.remove("x");
+    boardElement.classList.remove("circle");
+
+    if (isCircleTurn) {
+        boardElement.classList.add("x");
+    } else {
+        boardElement.classList.add("circle");
+    }
+};
+
+// Mudar o jogador
+const swapTurns = () => {
+    isCircleTurn = !isCircleTurn;
+};
+
+for (const cell of cellElements) {
+    cell.addEventListener("click", handleClick, { once: true }); //permitir clicar uma vez
+}
+
+function handleClick(element) {
+    // Marcar na célula (x ou círculo)
+    const cell = element.target; //target faz referencia ao elemento clicado
+    const classToAdd = isCircleTurn ? "circle" : "x";
+
+    placeMark(cell, classToAdd);
+    // Verificar vitória
+    // Verificar empate
+    // Mudar jogador
+
+    swapTurns();
+}
